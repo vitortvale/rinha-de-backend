cefc4bfd373cc6111dc42309b3533c06371270a2
Vitor Vale <vitortvale@gmail.com>
2025-08-07 21:23:01 -0300

    feat(memory): removed node_modules

--- app/node_modules/@nestjs/platform-fastify/constants.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const FASTIFY_ROUTE_CONFIG_METADATA = "__fastify_route_config__";
-export declare const FASTIFY_ROUTE_CONSTRAINTS_METADATA = "__fastify_route_constraints__";
-export declare const FASTIFY_ROUTE_SCHEMA_METADATA = "__fastify_route_schema__";
--- app/node_modules/@nestjs/platform-fastify/decorators/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './route-config.decorator';
-export * from './route-constraints.decorator';
-export * from './route-schema.decorator';
